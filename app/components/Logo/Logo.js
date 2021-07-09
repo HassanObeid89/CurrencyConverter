5537dbd94fb9c4abac246e9caeab3e57f710bc4b
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-07-09 09:57:53 +0200

    Animated the logo when the keyboard pops up, and avoided the keyboard to show on the top of the textinputs and buttons.

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,14 +1,67 @@
-import React from 'react';
-import {View, Image, Text} from 'react-native'
+import React, { Component } from 'react';
+import {View, Text, Keyboard, Animated, Platform} from 'react-native'
 import styles from './styles'
 
-const Logo = () =>(
-    <View style={styles.container}>
-        <Image style={styles.containerImage} source={require('./images/logo.png')}>
+class Logo extends Component {
 
-        </Image>
-        <Text style={styles.text} >Currency Converter</Text>
-    </View>
-);
+    constructor(props) {
+        super(props)
+
+        this.containerImageWidth = new Animated.Value(styles.$largeContainerSize)
+    }
+    componentDidMount() {
+        let showListener = 'keyboardWillShow'
+        let hideListener = 'keyboardWillHide'
+        if (Platform.OS === 'android') {
+            showListener = 'keyboardDidShow'
+            hideListener = 'keyboardDidHide'
+        }
+
+        this.keyboardShowListener = Keyboard.addListener(showListener, this.keyboardShow)
+        this.keyboardHideListener = Keyboard.addListener(hideListener, this.keyboardHide)
+    }
+
+    componentWillUnmount() {
+        this.keyboardShowListener.remove()
+        this.keyboardHideListener.remove()
+    }
+
+    keyboardShow = () => {
+        
+        Animated.parallel([
+
+            Animated.timing(this.containerImageWidth, {
+                toValue: styles.$smallContainerSize,
+            })
+        ]).start();
+     }
+
+    keyboardHide = () => {
+        Animated.parallel([
+
+            Animated.timing(this.containerImageWidth, {
+                toValue: styles.$largeContainerSize,
+            })
+        ]).start();
+    }
+
+    render() {
+
+        const containerImageStyle = [
+            styles.containerImage,
+            {width: this.containerImageWidth, height: this.containerImageWidth },
+        ]
+
+        return (
+            <View style={styles.container}>
+                <Animated.Image
+                    style={containerImageStyle}
+                source={require('./images/logo.png')}
+                />
+                <Text style={styles.text} >Currency Converter</Text>
+            </View>
+        )
+    }
+}
 
 export default Logo;
