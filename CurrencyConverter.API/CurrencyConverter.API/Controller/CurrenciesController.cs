254d070f3853dc7ba9521729fb1628171796239c
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-07-27 16:33:58 +0200

    Added C# endpoint

---
+++ CurrencyConverter.API/CurrencyConverter.API/Controller/CurrenciesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using CurrencyConverter.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace CurrencyConverter.API.Controller
+{
+    [ApiController]
+    [Route("api/rates")]
+    
+    public class CurrenciesController : ControllerBase
+    {
+        [HttpGet()]
+        public JsonResult GetRates()
+        {
+           
+            
+            return new JsonResult(RatesDataStore.GetRatesAsync());
+            
+        }
+
+    }
+}
---
+++ CurrencyConverter.API/CurrencyConverter.API/Model/Rates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace CurrencyConverter.API.Model
+{
+    public class CurrenciesRates
+    {
+
+        public Boolean Success { get; set; }
+        public string Base { get; set; }
+        public object Rates { get; set; }
+
+    }
+}
